c721b24f65ce9fc5675552b648ab9219a3c5b5a5
dmystical-coder <abdulsalam.baruwa190541028@st.lasu.edu.ng>
2022-06-24 12:42:53 +0100

    Final code submission

---
+++ app.js
@@ -0,0 +1,9 @@
+// Variable initialization and declaration 
+var myName = 'Abdulsalam', height = '6.2ft', country = 'Nigeria';
+
+// Print out name, height and country to the screen
+document.getElementById('name').innerHTML= 'Name: ' + myName;
+
+document.getElementById('height').innerHTML ='Height: ' + height;
+
+document.getElementById('country').innerHTML = 'Country: ' + country;
